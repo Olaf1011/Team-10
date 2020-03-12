ab8674e7fca1b32a385eca715d46c57d9d19b3e8
PHolmes1011 <61152948+PHolmes1011@users.noreply.github.com>
2020-03-12 22:29:46 +0000

    Fixed version of this script
    Previously I had uploaded the wrong version. This is the right one!

--- Assets/Scripts/AI/EnemySystem.cs
+++ Assets/Scripts/AI/EnemySystem.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EnemySystem : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private GameObject hitbox;
 
     [SerializeField] float speed = 250;
+    [SerializeField] float rotSpeed = 6;
+    [SerializeField] float stoppingDistance = 1;
 
     private Rigidbody2D _rb;
+
+    enum enemyState { CHASING, ATTACKING };
+    private enemyState state = enemyState.CHASING;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +26,58 @@ public class EnemySystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float dist = Vector3.Distance(player.position, transform.position);
+
         LookAtPlayer();
-        Move();
+        DistCheck(dist);
+
+        switch (state)
+        {
+            case enemyState.CHASING:
+                Move();
+                break;
+            case enemyState.ATTACKING:
+                _rb.velocity = Vector3.zero;
+
+                spawnHitbox();
+
+                if (dist > stoppingDistance)
+                    state = enemyState.CHASING;
+                break;
+        }
+
     }
 
     void LookAtPlayer()
     {
-        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position, Vector3.forward);
-        transform.rotation = rotation;
+        //Gets the rotation
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position, Vector3.forward).normalized;
 
+        //Rotate over time according to speed until in the required rotation
+        //A slerp is like a lerp but for rotation (turns it into a from and to vector, rather than points in the world)
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotSpeed);
+
+        //Stops the sprite wobbling
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
     }
 
     void Move()
     {
-        var movement = Vector3.forward;
         //moving the character by adding velocity
-        _rb.velocity = movement * speed * Time.deltaTime;
+        _rb.velocity = -transform.up * speed * Time.deltaTime;
+    }
+
+    //Distance check between enemy and player
+    void DistCheck(float dist)
+    {
+        if (dist < stoppingDistance)
+            state = enemyState.ATTACKING;
+        else
+            state = enemyState.CHASING;
+    }
+
+    void spawnHitbox()
+    {
+        Instantiate(hitbox, transform.position, transform.rotation);
     }
 }
